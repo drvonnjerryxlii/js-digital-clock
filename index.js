16ff0dc2f1ef3ed67c27a352c0d97c72d29ed082
Jeri <drvonnjerryxlii@gmail.com>
2015-08-25 14:40:13 -0700

    added clocks for us west, us east, sk seoul, & de berlin

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+function getTime(whatZone) {
+  switch (whatZone) {
+    case "us-east":
+      return new Date().toLocaleString("EN-US", { timeZone: "America/New_York" });
+    case "sk-seoul":
+      return new Date().toLocaleString("KO", { timeZone: "Asia/Seoul" });
+    case "us-west":
+      return new Date().toLocaleString("EN-US", { timeZone: "America/Los_Angeles" });
+    case "de-berlin":
+      return new Date().toLocaleString("DE", { timeZone: "Europe/Berlin" });
+  }
+}
+
+function displayTime() {
+  var zones = ["us-east", "sk-seoul", "us-west", "de-berlin"];
+  for (var i = 0; i < zones.length; i++) {
+    // set the current zone
+    var currentZone = zones[i];
+
+    // get the time for the current zone
+    var rightNow = getTime(currentZone);
+
+    // display the time on the page
+    var clock = document.getElementById(currentZone).innerHTML = rightNow;
+  }
+}
+
+// update the displayed time on a regular basis
+function updateTime() {
+  window.setInterval(displayTime, 500);
+}
